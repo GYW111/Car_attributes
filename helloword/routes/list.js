baa4cd3918be353d5ff9b5df36a33fcb1326eb3e
GYW111型 <3176698851@qq.com>
2023-12-11 18:34:40 +0800

    11

--- helloword/routes/list.js
+++ helloword/routes/list.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../Controller/userController');
 
 // 获取汽车列表
-router.get('/user', userController.getCarList);
+router.get('/car', userController.getCarList);
 router.get('/del', userController.deleCar);
 router.post('/add', userController.addCar);
 
